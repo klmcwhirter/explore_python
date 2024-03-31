22eebc702bbe3b82cf355995af8772d5deb95fea
Kevin McWhirter <klmcw@yahoo.com>
2024-03-31 16:13:58 -0700

    Add output via tabulate; move infrastructure to conftest.py

---
+++ tests/path/conftest.py
@@ -0,0 +1,67 @@
+
+from collections import defaultdict
+from typing import Mapping
+
+import pytest
+
+
+def gen_table(table: list[object]) -> str:
+    rc = '\n\nCaptured timings (secs):\n'
+
+    from tabulate import tabulate
+    rc += tabulate(
+        table,
+        headers='keys',
+        tablefmt='rounded_grid',
+        intfmt=',',
+    )
+
+    return rc
+
+
+def pivot_timings(captured_timings: Mapping[int, list[dict]]) -> list[dict]:
+    """ pivot timings by iterations, name"""
+
+    headers: list[str] = [
+        'Iterations',
+        *sorted([
+            event['name']
+            for k, grp in captured_timings.items() if k == 2_000_000
+            for event in grp
+        ])
+    ]
+
+    pivot = [
+        {
+            'Iterations': iteration,
+            **{
+                h: event['time_taken'] if event and 'time_taken' in event else ''
+                for h in headers[1:]
+                for event in grp if event['name'] == h
+            }
+        }
+        for iteration, grp in captured_timings.items()
+    ]
+
+    return pivot
+
+
+@pytest.fixture(scope='module')
+def timings():
+    rc: Mapping[int, list[dict]] = defaultdict[int, list[dict]](list[dict])
+    yield rc
+
+    pivot = pivot_timings(rc)
+    table = gen_table(pivot)
+    print(table)
+
+
+@pytest.fixture(scope='module')
+def capture_timing():
+    def wrapper(number: int, time_taken: float, name: str, timings_map: Mapping[int, list[dict]]):
+        timings_map[number].append({
+            'name': name,
+            'number': number,
+            'time_taken': time_taken
+        })
+    return wrapper
--- tests/path/test_path_manip_perf.py
+++ tests/path/test_path_manip_perf.py
@@ -3,7 +3,6 @@ import gc
 import os
 import time
 import timeit
-from collections import defaultdict
 from contextlib import contextmanager
 from typing import Callable
 
@@ -40,43 +39,6 @@ def test_slice_when_no_ext_should_give_root_path(image_path) -> None:
 # region perf tests
 
 
-def slice_with_concat() -> None:
-    image = SOME_IMG_PATH
-    _ = image[:image.rfind('.')] + MAP_EXT
-
-
-def slice_with_fstring() -> None:
-    image = SOME_IMG_PATH
-    _ = f'{image[:image.rfind('.')]}{MAP_EXT}'
-
-
-def path_with_concat() -> None:
-    _ = os.path.splitext(SOME_IMG_PATH)[0] + MAP_EXT
-
-
-def path_with_fstring() -> None:
-    image_root = os.path.splitext(SOME_IMG_PATH)[0]
-    _ = f'{image_root}{MAP_EXT}'
-
-
-@pytest.fixture(scope='module')
-def timings():
-    rc = defaultdict[str, list[object]](list[object])
-    yield rc
-
-    print('\n\ntimings captured:')
-
-    from pprint import pprint
-    pprint(rc, sort_dicts=True, width=130, underscore_numbers=True)
-
-
-def capture_timing(number: int, time_taken: int, name: str, timings: defaultdict[str, list[object]]) -> None:
-    timings[name].append({
-        'number': number,
-        'time_taken_ns': time_taken
-    })
-
-
 @contextmanager
 def force_gc():
     yield
@@ -87,65 +49,89 @@ def force_gc():
 
 
 @pytest.fixture
-def timeit_setup():
+def gc_setup():
     def wrapper(gc_enable: bool) -> str:
         return 'gc.enable()' if gc_enable else ''
 
     return wrapper
 
 
+def slice_w_concat() -> None:
+    image = SOME_IMG_PATH
+    _ = image[:image.rfind('.')] + MAP_EXT
+
+
+def slice_w_fstring() -> None:
+    image = SOME_IMG_PATH
+    _ = f'{image[:image.rfind('.')]}{MAP_EXT}'
+
+
+def path_w_concat() -> None:
+    _ = os.path.splitext(SOME_IMG_PATH)[0] + MAP_EXT
+
+
+def path_w_fstring() -> None:
+    image_root = os.path.splitext(SOME_IMG_PATH)[0]
+    _ = f'{image_root}{MAP_EXT}'
+
+
 @pytest.mark.parametrize(
     'number,func,gc_enable',
     [
-        (2_000_000, path_with_concat, True),
-        pytest.param(10_000_000, path_with_concat, True, marks=pytest.mark.slow),
-        pytest.param(25_000_000, path_with_concat, True, marks=pytest.mark.slow),
-        pytest.param(50_000_000, path_with_concat, True, marks=pytest.mark.slowest),
+        # With gc.enable()
 
-        (2_000_000, path_with_concat, False),
-        pytest.param(10_000_000, path_with_concat, False, marks=pytest.mark.slow),
-        pytest.param(25_000_000, path_with_concat, False, marks=pytest.mark.slow),
-        pytest.param(50_000_000, path_with_concat, False, marks=pytest.mark.slowest),
+        (2_000_000, path_w_concat, True),
+        pytest.param(10_000_000, path_w_concat, True, marks=pytest.mark.slow),
+        pytest.param(25_000_000, path_w_concat, True, marks=pytest.mark.slow),
+        pytest.param(50_000_000, path_w_concat, True, marks=pytest.mark.slowest),
 
-        (2_000_000, path_with_fstring, True),
-        pytest.param(10_000_000, path_with_fstring, True, marks=pytest.mark.slow),
-        pytest.param(25_000_000, path_with_fstring, True, marks=pytest.mark.slow),
-        pytest.param(50_000_000, path_with_fstring, True, marks=pytest.mark.slowest),
+        (2_000_000, path_w_fstring, True),
+        pytest.param(10_000_000, path_w_fstring, True, marks=pytest.mark.slow),
+        pytest.param(25_000_000, path_w_fstring, True, marks=pytest.mark.slow),
+        pytest.param(50_000_000, path_w_fstring, True, marks=pytest.mark.slowest),
 
-        (2_000_000, path_with_fstring, False),
-        pytest.param(10_000_000, path_with_fstring, False, marks=pytest.mark.slow),
-        pytest.param(25_000_000, path_with_fstring, False, marks=pytest.mark.slow),
-        pytest.param(50_000_000, path_with_fstring, False, marks=pytest.mark.slowest),
+        (2_000_000, slice_w_concat, True),
+        (10_000_000, slice_w_concat, True),
+        pytest.param(25_000_000, slice_w_concat, True, marks=pytest.mark.slow),
+        pytest.param(50_000_000, slice_w_concat, True, marks=pytest.mark.slow),
 
-        (2_000_000, slice_with_concat, True),
-        (10_000_000, slice_with_concat, True),
-        pytest.param(25_000_000, slice_with_concat, True, marks=pytest.mark.slow),
-        pytest.param(50_000_000, slice_with_concat, True, marks=pytest.mark.slow),
+        (2_000_000, slice_w_fstring, True),
+        (10_000_000, slice_w_fstring, True),
+        pytest.param(25_000_000, slice_w_fstring, True, marks=pytest.mark.slow),
+        pytest.param(50_000_000, slice_w_fstring, True, marks=pytest.mark.slow),
 
-        (2_000_000, slice_with_concat, False),
-        (10_000_000, slice_with_concat, False),
-        pytest.param(25_000_000, slice_with_concat, False, marks=pytest.mark.slow),
-        pytest.param(50_000_000, slice_with_concat, False, marks=pytest.mark.slow),
+        # With no gc
 
-        (2_000_000, slice_with_fstring, True),
-        (10_000_000, slice_with_fstring, True),
-        pytest.param(25_000_000, slice_with_fstring, True, marks=pytest.mark.slow),
-        pytest.param(50_000_000, slice_with_fstring, True, marks=pytest.mark.slow),
+        (2_000_000, path_w_concat, False),
+        pytest.param(10_000_000, path_w_concat, False, marks=pytest.mark.slow),
+        pytest.param(25_000_000, path_w_concat, False, marks=pytest.mark.slow),
+        pytest.param(50_000_000, path_w_concat, False, marks=pytest.mark.slowest),
 
-        (2_000_000, slice_with_fstring, False),
-        (10_000_000, slice_with_fstring, False),
-        pytest.param(25_000_000, slice_with_fstring, False, marks=pytest.mark.slow),
-        pytest.param(50_000_000, slice_with_fstring, False, marks=pytest.mark.slow),
+        (2_000_000, path_w_fstring, False),
+        pytest.param(10_000_000, path_w_fstring, False, marks=pytest.mark.slow),
+        pytest.param(25_000_000, path_w_fstring, False, marks=pytest.mark.slow),
+        pytest.param(50_000_000, path_w_fstring, False, marks=pytest.mark.slowest),
+
+        (2_000_000, slice_w_concat, False),
+        (10_000_000, slice_w_concat, False),
+        pytest.param(25_000_000, slice_w_concat, False, marks=pytest.mark.slow),
+        pytest.param(50_000_000, slice_w_concat, False, marks=pytest.mark.slow),
+
+        (2_000_000, slice_w_fstring, False),
+        (10_000_000, slice_w_fstring, False),
+        pytest.param(25_000_000, slice_w_fstring, False, marks=pytest.mark.slow),
+        pytest.param(50_000_000, slice_w_fstring, False, marks=pytest.mark.slow),
     ]
 )
 def test_slice_capture_timing(number: int, func: Callable[[], None], gc_enable: bool,
-                              timeit_setup, timings) -> None:
+                              gc_setup, timings, capture_timing) -> None:
+    # level the playing field with regards to gc
     with force_gc():
-        timer = timeit.Timer(stmt=func, setup=timeit_setup(gc_enable), timer=time.perf_counter_ns)
+        timer = timeit.Timer(stmt=func, setup=gc_setup(gc_enable), timer=time.perf_counter)
 
         time_taken = timer.timeit(number)
 
         gc = '_gc' if gc_enable else ''
-        capture_timing(number=number, time_taken=time_taken, name=f'{func.__name__}{gc}', timings=timings)
+        capture_timing(number=number, time_taken=time_taken, name=f'{func.__name__}{gc}', timings_map=timings)
 
 # endregion perf tests
